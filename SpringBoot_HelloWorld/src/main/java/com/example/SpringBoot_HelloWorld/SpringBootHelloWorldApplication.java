8d0f135637ce6490994b381520fb0019316ded12
pashmash372 <abhilashsivasanker@gmail.com>
2023-02-21 17:45:44 +0530

    code updated

---
+++ SpringBoot_HelloWorld/src/main/java/com/example/SpringBoot_HelloWorld/SpringBootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_HelloWorld/src/test/java/com/example/SpringBoot_HelloWorld/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
